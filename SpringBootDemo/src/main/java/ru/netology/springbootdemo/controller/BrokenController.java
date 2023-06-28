b00c02e5517a3d997d432e30bce579a8d705fa99
Misha <132915698+MichaelKlevcov@users.noreply.github.com>
2023-06-28 18:01:49 +0300

    Add files via upload

---
+++ SpringBootDemo/src/main/java/ru/netology/springbootdemo/controller/BrokenController.java
@@ -0,0 +1,27 @@
+package ru.netology.springbootdemo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import ru.netology.springbootdemo.demain.Person;
+
+@RestController
+@RequestMapping("/error")
+public class BrokenController {
+    @GetMapping("/{id}")
+    public Person getPersonById(@PathVariable("id") long id) {
+        try {
+            throw new IllegalArgumentException("IllegalArgumentException");
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ResponseStatusException", e);
+        }
+    }
+
+    @GetMapping("/iae")
+    public Person throwException() {
+        throw new IllegalArgumentException("throwException");
+    }
+}
