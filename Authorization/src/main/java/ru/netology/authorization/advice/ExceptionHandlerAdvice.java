55d32b1f3386b1e0c553c0a95b986ab285fea3af
Misha <132915698+MichaelKlevcov@users.noreply.github.com>
2023-07-24 10:48:56 +0300

    Update ExceptionHandlerAdvice.java

--- Authorization/src/main/java/ru/netology/authorization/advice/ExceptionHandlerAdvice.java
+++ Authorization/src/main/java/ru/netology/authorization/advice/ExceptionHandlerAdvice.java
@@ -7,3 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionHandlerAdvice {
 }
 
+@ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> handler(Exception e) {
+        return null;
+    }
+}
